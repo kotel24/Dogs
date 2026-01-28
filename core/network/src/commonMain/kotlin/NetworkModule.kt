3a096e1b123430519bad912b9d8ff0112ca67739
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2026-01-28 22:08:39 +0300

    feat(core-network): implement Ktor client with reified extension and Koin module

---
+++ core/network/src/commonMain/kotlin/NetworkModule.kt
@@ -0,0 +1,34 @@
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val networkModule = module {
+
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            install(Logging) {
+                logger = Logger.SIMPLE
+                level = LogLevel.ALL
+            }
+        }
+    }
+    single<NetworkClient> {
+        KtorNetworkClient(
+            httpClient = get(),
+            baseUrl = "https://dog.ceo/api/"
+        )
+    }
+}
